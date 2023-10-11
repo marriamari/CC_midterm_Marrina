98529cfb2f79a4062fcf2114275ec410350d25ef
marri <mah9917@nyu.edu>
2023-10-11 15:06:21 -0400

    removed P5 folder

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,800);
+
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+  noStroke();
+  fill(255,0,0);
+  ellipse(mouseX,mouseY,0,0);
+  
+
+}
